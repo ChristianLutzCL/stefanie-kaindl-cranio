74027322caf1306a29414341e6fe89d43a37dbe1
Christian Lutz <chris@inspiredprogrammer.com>
2023-12-11 20:50:19 +0100

    * feat(page.tsx): add new page component for AngebotePreise * feat(page.tsx): add new page component for Impressum * feat(page.tsx): add new page component for Kontakt * refactor(page.tsx): remove unused components from Home page * feat(page.tsx): add new page component for UeberMich

---
+++ src/app/angebote-preise/page.tsx
@@ -0,0 +1,14 @@
+// components
+import { Navbar, Footer } from "@/components";
+
+// sections
+import Hero from "./../hero";
+import Content from "./../content";
+
+export default function AngebotePreise() {
+  return (
+    <>
+      <Content />
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-// components
-import { Navbar, Footer } from "@/components";
-
-// sections
 import Hero from "./hero";
 import Content from "./content";
-import Comments from "./comments";
-import BlogPosts from "./blog-posts";
 
-export default function Campaign() {
+export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Content />
-      <Comments />
-      <BlogPosts />
-      <Footer />
     </>
   );
 }
