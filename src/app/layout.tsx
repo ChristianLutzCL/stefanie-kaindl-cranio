9462fa2f895f00dd48b7472b375a339f4685788f
Christian Lutz <chris@inspiredprogrammer.com>
2023-12-11 20:49:59 +0100

    * chore(layout.tsx): remove unused font-awesome stylesheet import * feat(layout.tsx): add Navbar and Footer components to Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import { Layout } from "@/components";
+import { Footer, Layout, Navbar } from "@/components";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -10,9 +10,8 @@ const roboto = Roboto({
 });
 
 export const metadata: Metadata = {
-  title: "NextJS Tailwind Blog Post Details Page",
-  description:
-    "Download Tailwind Blog Page, a Free Template developed by Creative Tim. Based on Tailwind CSS and Material Tailwind, see the live demo on our site and start sharing your stories with the world.",
+  title: "Cranio by Stefanie Kaindl",
+  description: "Cranio by Stefanie Kaindl",
 };
 
 export default function RootLayout({
@@ -23,18 +22,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
         <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </head>
       <body className={roboto.className}>
         <Layout>
+          <Navbar />
           {children}
+          <Footer />
         </Layout>
       </body>
     </html>
