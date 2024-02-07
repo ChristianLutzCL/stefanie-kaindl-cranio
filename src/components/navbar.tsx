5e64e5b92ededc6463de2ef0e8203416a74b3118
Christian Lutz <chris@inspiredprogrammer.com>
2024-02-07 19:12:35 +0100

    * refactor(navbar.tsx): clean up import statements and fix formatting * feat(navbar.tsx): add support for responsive navigation menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,44 +1,42 @@
 'use client';
 
-import React from "react";
+import React from 'react';
 import {
   Navbar as MTNavbar,
   Collapse,
   IconButton,
   Typography,
-} from "@material-tailwind/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import Image from "next/image";
-import logo from "../../public/logos/sk_logo_6.png";
-import Link from "next/link";
-
+} from '@material-tailwind/react';
+import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline';
+import Image from 'next/image';
+import logo from '../../public/logos/sk_logo_6.png';
+import Link from 'next/link';
 
 const NAV_MENU = [
   {
     title: 'Home',
-    href: '/'
+    href: '/',
   },
   {
     title: 'Über Mich',
-    href: '/ueber-mich'
+    href: '/ueber-mich',
   },
   {
     title: 'Angebote & Preise',
-    href: '/angebote-preise'
+    href: '/angebote-preise',
   },
   {
     title: 'Kontakt',
-    href: '/kontakt'
-  }
-]
+    href: '/kontakt',
+  },
+];
 
-function NavItem({ title, href }: { title: string, href: string }) {
+function NavItem({title, href}: {title: string; href: string}) {
   return (
     <li>
       <Link
         href={href}
-        className="flex items-center gap-2 font-light font-inter transition-all duration-100 underline-offset-1 decoration-transparent decoration-0 hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-[#9dccccff] hover:text-[#9dccccff]"
-      >
+        className='font-inter flex items-center gap-2 font-light decoration-transparent decoration-0 underline-offset-1 transition-all duration-100 hover:text-[#9dccccff] hover:underline hover:decoration-[#9dccccff] hover:decoration-2 hover:underline-offset-4'>
         {title}
       </Link>
     </li>
@@ -53,10 +51,7 @@ export function Navbar() {
   }
 
   React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpen(false)
-    );
+    window.addEventListener('resize', () => window.innerWidth >= 960 && setOpen(false));
   }, []);
 
   React.useEffect(() => {
@@ -68,48 +63,48 @@ export function Navbar() {
       }
     }
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
     <MTNavbar
       fullWidth
       shadow={false}
-      color="transparent"
-      className={isScrolling ? "fixed top-0 z-50 border-0 bg-outer-space-800 transition-all duration-200" : "fixed top-0 z-50 border-0 bg-transparent transition-all duration-500"}
-    >
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center gap-4">
+      color='transparent'
+      className={
+        isScrolling
+          ? 'fixed top-0 z-50 border-0 bg-outer-space-800 transition-all duration-200'
+          : 'fixed top-0 z-50 border-0 bg-transparent transition-all duration-500'
+      }>
+      <div className='container mx-auto flex items-center justify-between'>
+        <div className='flex items-center gap-4'>
           <Link href={'/'}>
-            <Image src={logo} height="55" alt="logo" className="antialiased" />
+            <Image src={logo} height='55' alt='logo' className='antialiased' />
           </Link>
         </div>
         <ul
-          className={`ml-10 hidden items-center gap-6 lg:flex ${isScrolling ? "text-white" : "text-white"
-            }`}
-        >
+          className={`ml-10 hidden items-center gap-6 lg:flex ${isScrolling ? 'text-white' : 'text-white'}`}>
           {NAV_MENU.map((item) => (
             <NavItem key={item.title} href={item.href} title={item.title} />
           ))}
         </ul>
         <IconButton
-          variant="text"
+          variant='text'
           onClick={handleOpen}
-          color={isScrolling ? "white" : "white"}
-          className="ml-auto inline-block lg:hidden"
-        >
+          color={isScrolling ? 'white' : 'white'}
+          className='ml-auto inline-block lg:hidden'>
           {open ? (
-            <XMarkIcon strokeWidth={2} className="h-6 w-6" />
+            <XMarkIcon strokeWidth={2} className='h-6 w-6' />
           ) : (
-            <Bars3Icon strokeWidth={2} className="h-6 w-6" />
+            <Bars3Icon strokeWidth={2} className='h-6 w-6' />
           )}
         </IconButton>
       </div>
       <Collapse open={open}>
-        <div className="container bg-outer-space-800 mx-auto rounded-md py-4 px-6 mt-3 text-white">
-          <ul className="flex flex-col gap-4">
+        <div className='container mx-auto mt-3 rounded-md bg-outer-space-800 px-6 py-4 text-white'>
+          <ul className='flex flex-col gap-4'>
             {NAV_MENU.map((item) => (
               <NavItem key={item.title} href={item.href} title={item.title} />
             ))}
