b6889749286756e1cac5d9f24b575a8a0c16ff78
Christian Lutz <christian.lutz@invenio.net>
2024-02-07 17:23:09 +0100

    * chore(content.tsx): refactor content component in angebote-preise folder * chore(page.tsx): remove unused imports in kontakt and ueber-mich pages

--- src/app/angebote-preise/content.tsx
+++ src/app/angebote-preise/content.tsx
@@ -7,9 +7,16 @@ import logo from "../../../public/image/3.jpg";
 
 function Content() {
   return (
-    <section className="flex pt-40">
-      <div className="flex flex-grid justify-between mx-auto max-w-screen-2xl gap-y-12 items-center w-full">
-        
+    <section className="flex h-screen">
+        <div className="flex flex-col justify-center items-center w-full">
+        <Typography variant="h2" className="mt-8 mb-6 font-inter text-white">
+        Angebote & Preise
+        </Typography>
+        <div className="grid grid-cols-3 gap-4">
+          <div className="bg-white h-64 w-64 rounded-md shadow-md"></div>
+          <div className="bg-white h-64 w-64 rounded-md shadow-md"></div>
+          <div className="bg-white h-64 w-64 rounded-md shadow-md"></div>
+        </div>
       </div>
     </section>
   )
--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,8 +1,3 @@
-// components
-import { Navbar, Footer } from "@/components";
-
-// sections
-import Hero from "./../hero";
 import Content from "./../content";
 
 export default function Kontakt() {
